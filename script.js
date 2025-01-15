0f96f7a95f4c53211e16d134bdb2ffe1a4aff2de
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-01-15 13:09:19 +0100

    feat: user is able to remove book from library

--- script.js
+++ script.js
@@ -19,41 +19,12 @@ const myLibrary = [
   },
 ];
 
-function Book(title, author, pages, read) {
-  this.title = title;  
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-function addBookToLibrary(title, author, pages, read) {
-  const book = new Book(title, author, pages, read);
-  myLibrary.push(book);
-}
-function displayLibrary(library){
-  library.forEach(book =>{
-    displayBook(book);
-  });
-}
-
-function displayBook(book){
-  const tBody = document.querySelector("tbody");
-  const tRow = document.createElement("tr");
-  tBody.appendChild(tRow);
-  for(category in book) {
-    const tCell = document.createElement("td");
-    tCell.textContent = book[category];
-    tRow.appendChild(tCell);
-  }
-}
-
-displayLibrary(myLibrary);
 const showBtn = document.querySelector(".show-modal");
 const closeBtn = document.querySelector(".cancel");
-const confirmBtn = document.querySelector(".confirm")
 const dialog = document.querySelector("dialog")
 const form = document.querySelector("form");
 
+displayLibrary(myLibrary);
 // Dialog event listeners
 showBtn.addEventListener("click", () =>{
     dialog.showModal();
@@ -66,6 +37,58 @@ closeBtn.addEventListener("click", (e) => {
 form.addEventListener("submit", () =>{
   let book = document.querySelectorAll(["#title", "#author", "#pages", "#read"]);
   addBookToLibrary(book[0].value, book[1].value, book[2].value, book[3].value); 
-  displayBook(myLibrary[myLibrary.length - 1]);
+  displayBook(myLibrary[myLibrary.length - 1], myLibrary);
   form.reset();
 })
+
+function Book(title, author, pages, read) {
+  this.title = title;  
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+function displayLibrary(library){
+  clearTable();
+  library.forEach(book =>{
+    displayBook(book, library);
+  });
+}
+
+function displayBook(book, library){
+  const tBody = document.querySelector("tbody");
+  const tRow = document.createElement("tr");
+  const rmBtn = document.createElement("button");
+  tBody.appendChild(tRow);
+  for(category in book) {
+    const tCell = document.createElement("td");
+    tCell.textContent = book[category];
+    tRow.appendChild(tCell);
+  }
+  const tCell = document.createElement("td");
+  tRow.appendChild(tCell);
+  tCell.appendChild(rmBtn);
+  rmBtn.classList.add("remove-book");
+  rmBtn.setAttribute("data-attribute", library.map(item => item.title).indexOf(book.title));
+  rmBtn.textContent = "Remove book";
+  rmBtn.addEventListener("click", e => {
+    const index = rmBtn.getAttribute("data-attribute");
+    removeBook(index, myLibrary); 
+  });
+}
+
+function clearTable(){
+  const rows = document.querySelectorAll("tbody tr");
+  rows.forEach(row => row.remove());
+}
+
+function removeBook(index, library){
+  library.splice(index, 1);
+  displayLibrary(library);
+}
+
+
