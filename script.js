8adf61b9b0aecc23b80898bf4955825b5a332800
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-01-24 13:06:40 +0100

    style and layout: card

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const myLibrary = [
   {
-    title: "Lo Hobbit",
+    title: "Angela",
     author: "Fratm",
     pages: 295,
     read: "read",
+    description: ""
   },
   {
-    title: "Nosferatu",
+    title: "è",
     author: "Patt",
     pages: 245,
     read: "read",
   },
   {
-    title: "Hulk",
+    title: "bellissimamente",
+    author: "Sort",
+    pages: 198,
+    read: "not read yet",
+  },
+
+  {
+    title: "billissimissimisssimissima",
     author: "Sort",
     pages: 198,
     read: "not read yet",
@@ -41,11 +49,12 @@ form.addEventListener("submit", () =>{
   form.reset();
 })
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, description) {
   this.title = title;  
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.description = description;
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -59,7 +68,44 @@ function displayLibrary(library){
 }
 
 function displayBook(book, library){
+  const content = document.querySelector(".content");
+  const card = document.createElement("div");
+  card.classList.add("card");
   
+  const cardHeader = document.createElement("h2");
+  cardHeader.textContent = book.title;
+
+  const par = document.createElement("p");
+  par.textContent = book.description;
+
+  const btnsDiv = document.createElement("div");
+  btnsDiv.classList.add("card-buttons"); 
+
+  card.appendChild(cardHeader);
+  card.appendChild(par);
+  card.appendChild(btnsDiv);
+  
+  const btnRead = document.createElement("button");
+  const btnRm = document.createElement("button");
+
+  btnsDiv.appendChild(btnRead);
+  btnsDiv.appendChild(btnRm);
+
+  const imgRead = document.createElement("img");
+  const imgRm = document.createElement("img");
+  setAttributes(imgRead, {src:`images/book-open-blank-variant-outline.svg`, alt:"no-image", width:"18", height:"18"});
+  setAttributes(imgRm, {src:`images/book-remove.svg`, alt:"no-image", width:"18", height:"18"});
+  
+  btnRead.appendChild(imgRead);
+  btnRm.appendChild(imgRm);  
+  
+  content.appendChild(card);
+}
+
+function setAttributes(el, attr){
+  for(key in attr){
+    el.setAttribute(key, attr[key]);
+  }
 }
 
 function removeBook(index, library){
